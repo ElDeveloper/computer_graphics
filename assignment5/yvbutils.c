4c6eaa29031aed05abbf6ae1529a6d442528af85
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2013-10-10 16:13:35 -0600

    Add files for assignment 5
    Note that most of these files come from CSCIx229.h the library as
    provided in the CSCI5229 course of Fall 2013 @ CU Boulder.

---
+++ assignment5/yvbutils.c
@@ -0,0 +1,68 @@
+#include "CSCIx229.h"
+// A lot of the code in this file is heavily based on example 13 from CSSCI5229
+
+void draw_cylinder(double x, double y, double z, unsigned int steps, float colors[3][3]){
+	double circle_points[steps][2];
+	float di=360/steps, angle=0;
+
+	// ideally we don't want to change it, but leave it there ... just in case
+	static float r = 1, h = 1;
+	int i = 0;
+
+	// generate the points
+	for (i = 0; i<steps; i++ ){
+		angle+=di;
+
+		circle_points[i][0] = r*cos(i);
+		circle_points[i][1] = r*sin(i);
+	}
+
+	//  Save transformation
+	glPushMatrix();
+	glTranslated(x,y,z);
+
+	// other side
+	glBegin(GL_TRIANGLE_STRIP);
+	glNormal3f(0, 0, -1);
+	glColor3f(colors[0][0], colors[0][1], colors[0][2]);
+	for (i=0; i<steps; i++){
+		glVertex3d(0, 0, z);
+		glVertex3d(circle_points[i][0],circle_points[i][1], z);
+	}
+	glVertex3d(0, 0, z);
+	glVertex3d(circle_points[0][0],circle_points[0][1], z);
+	glEnd();
+
+	// one side
+	glBegin(GL_TRIANGLE_STRIP);
+	glNormal3f(0, 0, 1);
+	glColor3f(colors[1][0], colors[1][1], colors[1][2]);
+	for (i=0; i<steps; i++){
+		glVertex3d(0, 0, z+h);
+		glVertex3d(circle_points[i][0],circle_points[i][1], z+h);
+	}
+	glVertex3d(0, 0, z+h);
+	glVertex3d(circle_points[0][0],circle_points[0][1], z+h);
+	glEnd();
+
+	// draw the strip connecting the two circles using the differential value in
+	// height (Z) between the two circles; the value is the same for all other
+	glBegin(GL_QUAD_STRIP);
+	glColor3f(colors[2][0], colors[2][1], colors[2][2]);
+	for (i=0; i<steps; i++){
+		glNormal3f(circle_points[i][0], circle_points[i][1], 1);
+		glVertex3d(circle_points[i][0],circle_points[i][1], z);
+		glVertex3d(circle_points[i][0],circle_points[i][1], z+h);
+	}
+	glVertex3d(circle_points[0][0],circle_points[0][1], z);
+	glVertex3d(circle_points[0][0],circle_points[0][1], z+h);
+	glEnd();
+
+	//  Undo transformations
+	glPopMatrix();
+}
+
+void draw_house(double x, double y, double z, float colors[3][3]){
+	draw_cylinder(x, y, z, 5, colors);
+}
+
