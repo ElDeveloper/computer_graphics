1ca62d425df476fe5f5cd5005fac5ea05c93a73e
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2013-10-26 21:35:42 -0600

    Add first pass at assignment number 7
    Some textures are added to the scene but there are still a few to go,
    the remaining textures were added to this folder regardless of whether
    or not they are being used.

---
+++ assignment7/CSCIx229.h
@@ -0,0 +1,34 @@
+#ifndef CSCIx229
+#define CSCIx229
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <math.h>
+
+#ifdef USEGLEW
+#include <GL/glew.h>
+#endif
+#define GL_GLEXT_PROTOTYPES
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#define Cos(th) cos(3.1415926/180*(th))
+#define Sin(th) sin(3.1415926/180*(th))
+
+void Print(const char* format , ...);
+void Fatal(const char* format , ...);
+unsigned int LoadTexBMP(const char* file);
+void Project(double fov,double asp,double dim);
+void ErrCheck(const char* where);
+int  LoadOBJ(const char* file);
+
+void draw_cylinder(double x, double y, double z, unsigned int steps, float colors[3][3]);
+void draw_house(double x, double y, double z, float colors[3][3]);
+
+#endif
+
---
+++ assignment7/README.md
@@ -0,0 +1,17 @@
+Assignment 5
+============
+
+Drawing a random scene with both perspectives.
+
+- To rotate around the space use the arrow keys.
+- To change the field of view use `+` and `-`.
+- To show/hide the axes use `x`.
+- To reset back to the initial viewing position use `0`.
+- To switch between orthogonal and perspective use `p`.
+- To raise the position of the light use `]` to decrease it use `[`.
+- Move the light from one side to the other using `<` and `>`.
+- Toggle the lights on and off using `l`.
+- Toggle the movement of the light using `m`.
+- To exit graciously use `ESC`.
+
+Time it took me to complete 6 hours.
